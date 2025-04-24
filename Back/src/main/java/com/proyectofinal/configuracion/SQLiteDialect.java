f7a6604897969277ee67fe07a168d20f07e7ba6c
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-04-24 09:59:26 +0200

    añado clase Dialect

---
+++ Back/src/main/java/com/proyectofinal/configuracion/SQLiteDialect.java
@@ -0,0 +1,19 @@
+package com.proyectofinal.configuracion;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.identity.IdentityColumnSupport;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+
+public class SQLiteDialect extends Dialect {
+    @Override
+    public IdentityColumnSupport getIdentityColumnSupport() {
+        return new IdentityColumnSupportImpl();
+    }
+
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    // Puedes añadir más configuraciones necesarias si usas JPA
+}
