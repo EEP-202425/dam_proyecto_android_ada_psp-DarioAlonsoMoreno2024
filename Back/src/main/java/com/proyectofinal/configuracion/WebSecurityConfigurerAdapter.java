828d79a0a018f10112889709b451f012ae9dbc76
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-04-24 10:02:50 +0200

    añado WebSecurityConfigurerAdapter

---
+++ Back/src/main/java/com/proyectofinal/configuracion/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,12 @@
+package com.proyectofinal.configuracion;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+public class WebSecurityConfigurerAdapter {
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
