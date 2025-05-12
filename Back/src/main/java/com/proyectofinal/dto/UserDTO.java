6ea22d87ff549b1dc046df310c5cf1c8fe14f564
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-05-12 13:32:37 +0200

    modifico userDTO

--- Back/src/main/java/com/proyectofinal/dto/UserDTO.java
+++ Back/src/main/java/com/proyectofinal/dto/UserDTO.java
@@ -6,6 +6,8 @@ public class UserDTO {
 	private String email;
 	private String password;
 	private String name; // Si es necesario
+    private String lastName;   // ← falta
+    private Integer edad;
 
 	public String getEmail() {
 		return email;
@@ -30,12 +32,33 @@ public class UserDTO {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
 
 	public Usuario toUsuario() {
 		Usuario usuario = new Usuario();
 		usuario.setEmail(this.email);
-	//	usuario.setPassword(this.password); // Asegúrate de encriptar la contraseña
+		usuario.setPassword(this.password); // Asegúrate de encriptar la contraseña
 		usuario.setName(this.name);
+		usuario.setEdad(this.edad);
+		usuario.setLastName(this.lastName);
+		
 		return usuario;
 	}
 }
