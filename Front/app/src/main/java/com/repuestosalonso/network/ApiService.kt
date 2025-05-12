3856b13b54bd9d91902b4239e4dc61df73d5562d
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-05-12 13:36:30 +0200

    actualizo ApiService

--- Front/app/src/main/java/com/repuestosalonso/network/ApiService.kt
+++ Front/app/src/main/java/com/repuestosalonso/network/ApiService.kt
@@ -1,27 +1,39 @@
-package com.example.repuestosalonso.network
+package com.repuestosalonso.network
 
-import com.example.repuestosalonso.model.LoginRequest
-import com.example.repuestosalonso.model.LoginResponse
-import com.example.repuestosalonso.model.Pedido
-import com.example.repuestosalonso.model.Repuesto
+import com.repuestosalonso.model.LoginRequest
+import com.repuestosalonso.model.LoginResponse
+import com.repuestosalonso.model.Pedido
+import com.repuestosalonso.model.PedidoRequest
+import com.repuestosalonso.model.Repuesto
+import com.repuestosalonso.model.Usuario
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("login")
-    suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
 
-    @GET("repuestos")
-    suspend fun getRepuestos(@Header("Authorization") token: String): List<Repuesto>
+    // Listar todos los usuarios (si lo necesitas)
+    @GET("api/usuarios")
+    suspend fun getUsers(): Response<List<Usuario>>
 
-    @POST("pedidos")
+    // Crear usuario
+    @POST("api/usuarios/registro")
+    suspend fun crearUsuario(@Body usuario: Usuario): Response<Usuario>
+
+    // Login
+    @POST("api/usuarios/login")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
+
+    @GET("api/producto")
+    suspend fun getRepuestos(
+        @Header("Authorization") bearerToken: String
+    ): Response<List<Repuesto>>
+
+    @POST("api/pedidos")
     suspend fun crearPedido(
-        @Header("Authorization") token: String,
-        @Body pedido: Pedido
-    ): Pedido
-
-    @GET("pedidos")
-    suspend fun getPedidos(@Header("Authorization") token: String): List<Pedido>
+        @Header("Authorization") bearerToken: String,
+        @Body pedido: PedidoRequest
+    ): Response<Pedido>
 }
