b505421dde0f77b852449cb579c9d74012181ab6
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-05-13 21:37:28 +0200

    actualizo PedidoService

--- Back/src/main/java/com/proyectofinal/services/PedidoService.java
+++ Back/src/main/java/com/proyectofinal/services/PedidoService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.proyectofinal.dao.PedidoRepository;
+import com.proyectofinal.dao.ProductRepository;
 import com.proyectofinal.dao.UsuarioRepository;
 import com.proyectofinal.dominio.Pedido;
+import com.proyectofinal.dominio.Producto;
 import com.proyectofinal.dominio.Usuario;
 
 @Service
@@ -16,39 +18,33 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	 @Autowired
+	 private ProductRepository productoRepo;
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
 	@Autowired
 	private InventarioService inventarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepo;
 
 	public List<Pedido> obtenerTodos() {
 		return pedidoRepository.findAll();
 	}
 
-	public Pedido crearPedido(Long usuarioId) {
-		Usuario usuario = usuarioRepository.findById(usuarioId)
-				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+	  public Pedido crearPedido(Long usuarioId, Long productoId, int cantidad) {
+		    Usuario u  = usuarioRepo.findById(usuarioId)
+		                  .orElseThrow(() -> new RuntimeException("Usuario no existe"));
+		    Producto p = productoRepo.findById(productoId)
+		                  .orElseThrow(() -> new RuntimeException("Producto no existe"));
 
-		Pedido pedido = new Pedido();
-		pedido.setUsuario(usuario);
-		pedido.setFecha(LocalDate.now());
-
-		return pedidoRepository.save(pedido);
-	}
-
-	public Pedido crearPedido(Long usuarioId, Long productoId, int cantidad) {
-		Usuario usuario = usuarioRepository.findById(usuarioId)
-				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-		inventarioService.reducirStock(productoId, cantidad); // ¡resta stock!
-
-		Pedido pedido = new Pedido();
-		pedido.setUsuario(usuario);
-		pedido.setFecha(LocalDate.now());
-		// si agregamos producto/cantidad al pedido, también lo seteamos
-
-		return pedidoRepository.save(pedido);
-	}
-}
+		    Pedido pedido = new Pedido();
+		    pedido.setUsuario(u);
+		    pedido.setProducto(p);
+		    pedido.setFecha(LocalDate.now());
+		    return pedidoRepository.save(pedido);
+		  }
+		}
