9d44e82dccea9d1449cc49f9cf935cfc6ae49541
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-05-13 21:37:44 +0200

    actyualizo repuestosRepository

--- Front/app/src/main/java/com/repuestosalonso/data/RepuestosRepository.kt
+++ Front/app/src/main/java/com/repuestosalonso/data/RepuestosRepository.kt
@@ -1,8 +1,11 @@
 package com.repuestosalonso.data
 
+import android.util.Log
+import com.repuestosalonso.model.NewRepuestoRequest
+import com.repuestosalonso.model.PedidoRequest
+import com.repuestosalonso.model.PedidoResponse
 import com.repuestosalonso.model.Repuesto
 import com.repuestosalonso.network.RetrofitClient
-import com.repuestosalonso.network.RetrofitClient.apiService
 import retrofit2.Response
 
 class RepuestosRepository {
@@ -11,4 +14,32 @@ class RepuestosRepository {
     suspend fun fetchRepuestos(token: String): Response<List<Repuesto>> {
         return api.getRepuestos("Bearer $token")
     }
+
+    suspend fun makeOrder(
+        token: String,
+        userId: Long,
+        productId: Long,
+        count: Int
+    ): Response<PedidoResponse> {
+        val request = PedidoRequest(
+            userId =  userId,
+            productId = productId,
+            count  = count
+        )
+
+        Log.d("ORDER-REQ", "REQUEST JSON: $request")
+
+      return api.crearPedido("Bearer $token", request)
+    }
+    suspend fun addRepuesto(
+        token: String,
+        userId: Long,
+        nombre: String,
+        precio: Double,
+        year: Int
+    ): Response<Repuesto> {
+        val request = NewRepuestoRequest(nombre, precio, year, userId)
+        return api.crearRepuesto("Bearer $token", request)
+    }
+
 }
