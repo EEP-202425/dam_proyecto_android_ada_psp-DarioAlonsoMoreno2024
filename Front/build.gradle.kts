9b6e0034fa7a5d3869ecce1575aa6c3852d46474
DarioAlonsoMoreno2024 <dario-alonso1@eep-igroup.com>
2025-05-13 21:38:17 +0200

    librerias

--- Front/build.gradle.kts
+++ Front/build.gradle.kts
@@ -1,6 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+buildscript {
+    extra.apply {
+        set("lifecycle_version", "2.6.2")
+
+    }
 }
+    plugins {
+        id("com.android.application") version "8.8.0" apply false
+        id("com.android.library") version "8.8.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    }
